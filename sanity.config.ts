524fc9b36eb82e939421c53925a9ff28cce5c85a
polymaster16 <uili.kam8@gmail.com>
2023-11-20 03:26:46 +0100

    renamed:    .env.example -> .env.example.txt 	new file:   .sanity/runtime/app.js 	new file:   .sanity/runtime/index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,6 @@
 // Different environments use different variables
-const projectId =
-  import.meta.env.PUBLIC_SANITY_STUDIO_PROJECT_ID! ||
-  import.meta.env.PUBLIC_SANITY_PROJECT_ID!;
-const dataset =
-  import.meta.env.PUBLIC_SANITY_STUDIO_DATASET! ||
-  import.meta.env.PUBLIC_SANITY_DATASET!;
+const projectId = 'm93p5d26'
+const dataset = 'production'
 
 // Feel free to remove this check if you don't need it
 if (!projectId || !dataset) {
